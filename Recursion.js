aece6c290a8d76ce6008c34825aa270253444a52
nikitha26 <mnikitha365@gmail.com>
2022-07-23 21:27:06 +0530

    recursion problems

--- Recursion.js
+++ Recursion.js
@@ -33,7 +33,7 @@ function factorialByRecursive(num) {
     }
     return num * factorialByRecursive(num-1);
 }
-console.log(factorialByRecursive(4))
+//console.log(factorialByRecursive(4))
 
 
 
@@ -43,7 +43,7 @@ function abcd(num) {
     }
     return num * abcd(num - 1);
 }
-console.log(abcd(5))
+//console.log(abcd(5))
 
 
 //Collect Odd Values in an array with helper method
@@ -63,4 +63,60 @@ function findOddValue(arr) {
     helper(arr)
     return result;
 }
-console.log(findOddValue([1,2,3,4,5,6]))
+//console.log(findOddValue([1,2,3,4,5,6]))
+
+function power(n1,n2) {
+    if(n2 === 0){
+        return 1;
+    }
+    return n1 * power(n1,n2-1);
+}
+//console.log(power(2,2));
+
+function fac(n1) {
+    if(n1 === 1 || n1 ===0){
+        return 1;
+    }
+    return n1 * fac(n1-1);
+}
+//console.log(fac(7));
+
+function product(arr) {
+    // var res = arr.reduce(function(a,b){
+    //    return a*b;
+    // })
+    // return res;
+    // let res = 1;
+    // for(let i = 0;i < arr.length;i++){
+    //    res *= arr[i];
+    // }
+    // return res;
+
+    if(arr.length === 0){
+       return 1;
+    }
+    return arr[0] * product(arr.slice(1));
+}
+//console.log(product([1,2,3,10]))
+
+function fib(n){
+    if (n <= 2) return 1;
+    return fib(n-1) + fib(n-2);
+}
+//console.log(fib(4))
+function reverse(str) {
+    if(str.length <= 1){
+       return str;
+    }
+    //return str.split('').reverse().join('');
+    return reverse(str.slice(1)) +" " +str[0];
+                    // wesome + a;
+                    // esome + w;
+                    // some + e;
+                    // ome + s;
+                    // me + o;
+                    // e + m;
+                    // '' + e;
+
+}
+console.log(reverse('awesome'))
